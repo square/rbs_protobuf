a3f2d08791bec594b60d352a8262ab5deda00056
Soutaro Matsumoto <soutaro@squareup.com>
2020-07-01 09:33:23 +0900

    Fix mktmpdir

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
 require "rbs_protobuf"
 
 require "minitest/autorun"
@@ -6,28 +7,19 @@ require "tmpdir"
 require "open3"
 
 module TestHelper
-  def read_code_generator_request(proto_path)
-    full_path = File.join(__dir__, "../examples", proto_path)
-    dumper_path = File.join(__dir__, "../bin/protoc-gen-dumper")
+  def tmpdir_path
+    File.join(__dir__, "../tmp")
+  end
 
-    Dir.mktmpdir do |dir|
-      Dir.chdir(dir) do
-        Open3.capture2("protoc",
-                       "--plugin=#{dumper_path}",
-                       "--dumper_out=/",      # --dumper_out is ignored
-                       "--proto_path=#{File.join(__dir__, "../examples")}",
-                       full_path)
-      end
-
-      content = File.read(File.join(dir, "a.pb.out"))
-      Google::Protobuf::Compiler::CodeGeneratorRequest.decode(content)
-    end
+  def setup
+    super
+    Dir.mkdir(tmpdir_path) unless Dir.exist?(tmpdir_path)
   end
 
   def read_proto(proto)
     dumper_path = File.join(__dir__, "../bin/protoc-gen-dumper")
 
-    Dir.mktmpdir do |dir|
+    Dir.mktmpdir(nil, tmpdir_path) do |dir|
       File.write(File.join(dir, "a.proto"), proto)
 
       Dir.chdir(dir) do
